5a70c7f24d8cfde76641e907d196ca4d3ea3854e
Matt Chase <mattchase@Daedalus.local>
2025-07-18 20:57:55 -0400

    Add debug

---
+++ engine/debug/index.js
@@ -0,0 +1,77 @@
+import GUI from 'lil-gui';
+import Stats from "three/addons/libs/stats.module.js";
+import { addAction, doAction } from '../hooks';
+
+import { rootScene } from '../scene-manager/';
+
+const stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+
+addAction(
+  'gameController.update',
+  'gameController.update/stats',
+  () => {
+    stats.update();
+  }
+);
+
+const params = {
+  LogScene: () => console.log(rootScene.children[0]),
+  LogActions: () => console.log(getHooks().actions),
+  DebugAction: 'debug.timelineTest',
+  ExecDebugAction: () => doAction(params.DebugAction)
+};
+
+const gui = new GUI();
+
+const consoleGui = gui.addFolder('Console');
+consoleGui.add(params, 'LogScene');
+consoleGui.add(params, 'LogActions');
+consoleGui.add(params, 'DebugAction');
+consoleGui.add(params, 'ExecDebugAction');
+consoleGui.close();
+
+const lightingGui = gui.addFolder('Lights');
+lightingGui.close();
+
+addAction(
+  'gltf.load',
+  'gltf.load/addLightingGUI',
+  (gltf) => {
+    lightingGui.foldersRecursive().forEach((F) => F.destroy());
+
+    const { scene } = gltf;
+    scene.traverse((light) => {
+      if (!light.isLight) {
+        return;
+      }
+
+      const f = lightingGui.addFolder(light.name);
+      f.addColor(light, 'color');
+      f.add(light, 'intensity', 0, light.type === 'AmbientLight' ? 2 : 255);
+      f.add(light, 'castShadow');
+      f.close();
+    });
+  }
+);
+
+const fogGui = gui.addFolder('Fog');
+fogGui.close();
+
+addAction(
+  'gltf.load',
+  'gltf.load/addFogGUI',
+  (gltf) => {
+
+    setTimeout(() => {
+      const { fog } = rootScene;
+      fogGui.controllersRecursive().forEach((C) => C.destroy());
+
+      fogGui.addColor(fog, 'color');
+      fogGui.add(fog, 'near', 0, 255);
+      fogGui.add(fog, 'far', 0, 255);
+    }, 10);
+  }
+);
